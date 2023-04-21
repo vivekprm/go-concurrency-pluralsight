e7fb6d42cf8b33c2e36c0b99c06423e5fd1dfac9
Vivek Mishra <v.mishra@f5.com>
2023-04-21 13:51:56 +0530

    for loop demo

--- main.go
+++ main.go
@@ -12,13 +12,16 @@ func main() {
 	wg.Add(2)
 
 	go func (ch chan int, wg *sync.WaitGroup)  {
-		if msg, ok := <- ch; ok {
-			fmt.Println(msg, ok)
+		for msg := range ch {
+			fmt.Println(msg)
 		}
 		wg.Done()
 	}(ch, wg)
 
 	go func (ch chan int, wg *sync.WaitGroup)  {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
 		close(ch)
 		wg.Done()
 	}(ch, wg)
